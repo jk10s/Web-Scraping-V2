591812291cc479b5d51fbda097f9d8bc89b0a78c
jk10s <juanka_267@hotmail.com>
2023-01-17 23:57:04 -0500

    git maximin selenium

---
+++ selenium/maximizado_selenium.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome(chrome_options=options)
+driver.get("https://www.google.com")
